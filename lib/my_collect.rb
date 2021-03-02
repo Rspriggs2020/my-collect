d62b30d07b55ba38228efc8de1d17ca48bc60f7c
ryliespriggs <rspriggs812@gmail.com>
2021-03-01 16:42:07 -0800

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(array)
+    x = 0
+    collection = []
+    while x < array.length
+        yield(array[x])
+        collection << yield(array[x])
+        x += 1
+    end
+    collection
 
+    
+end
 
